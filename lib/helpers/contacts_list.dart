8d06dc8fe8acf922d700f6e8300d0296284b874d
HaudegenHH <einsundeins79@gmx.de>
2023-03-12 15:49:50 +0100

    finished create group page

--- lib/helpers/contacts_list.dart
+++ lib/helpers/contacts_list.dart
@@ -7,7 +7,6 @@ List<ChatModel> contacts = [
     isGroup: true,
     currentMessage: "Who am i to diss a brie?",
     savedContacts: true,
-    selected: true,
   ),
   ChatModel(
     name: "Emma",
@@ -22,7 +21,58 @@ List<ChatModel> contacts = [
         "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Daniel_Radcliffe_SDCC_2014.jpg/800px-Daniel_Radcliffe_SDCC_2014.jpg",
     currentMessage: "Hello, i m using WhatsApp",
     savedContacts: true,
-    selected: true,
+  ),
+  ChatModel(
+      name: "Emil",
+      imgUrl: "",
+      currentMessage: "Never eat yellow snow!",
+      savedContacts: true),
+  ChatModel(
+    name: "Anna",
+    imgUrl: "",
+    isGroup: true,
+    currentMessage: "Who am i to diss a brie?",
+    savedContacts: true,
+  ),
+  ChatModel(
+    name: "Emma",
+    imgUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/7/7f/Emma_Watson_2013.jpg",
+    currentMessage: "Love will make your day",
+    savedContacts: true,
+  ),
+  ChatModel(
+    name: "Daniel",
+    imgUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Daniel_Radcliffe_SDCC_2014.jpg/800px-Daniel_Radcliffe_SDCC_2014.jpg",
+    currentMessage: "Hello, i m using WhatsApp",
+    savedContacts: true,
+  ),
+  ChatModel(
+      name: "Emil",
+      imgUrl: "",
+      currentMessage: "Never eat yellow snow!",
+      savedContacts: true),
+  ChatModel(
+    name: "Anna",
+    imgUrl: "",
+    isGroup: true,
+    currentMessage: "Who am i to diss a brie?",
+    savedContacts: true,
+  ),
+  ChatModel(
+    name: "Emma",
+    imgUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/7/7f/Emma_Watson_2013.jpg",
+    currentMessage: "Love will make your day",
+    savedContacts: true,
+  ),
+  ChatModel(
+    name: "Daniel",
+    imgUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Daniel_Radcliffe_SDCC_2014.jpg/800px-Daniel_Radcliffe_SDCC_2014.jpg",
+    currentMessage: "Hello, i m using WhatsApp",
+    savedContacts: true,
   ),
   ChatModel(
       name: "Emil",
--- lib/screens/create_group_page.dart
+++ lib/screens/create_group_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/helpers/contacts_list.dart';
+import 'package:whatsapp_clone/models/chat_model.dart';
 import 'package:whatsapp_clone/widgets/contact_group_card.dart';
-import 'package:whatsapp_clone/widgets/custom_card.dart';
 
 class CreateNewGroupPage extends StatefulWidget {
   const CreateNewGroupPage({super.key});
@@ -11,6 +11,22 @@ class CreateNewGroupPage extends StatefulWidget {
 }
 
 class _CreateNewGroupPageState extends State<CreateNewGroupPage> {
+  List<ChatModel> group = [];
+
+  void addToGroup(contact) {
+    setState(() {
+      contact.selected = true;
+      group.add(contact);
+    });
+  }
+
+  void removeFromGroup(contact) {
+    setState(() {
+      contact.selected = false;
+      group.remove(contact);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,20 +57,116 @@ class _CreateNewGroupPageState extends State<CreateNewGroupPage> {
           ),
         ],
       ),
-      body: ListView.builder(
-        shrinkWrap: true,
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          final contact = contacts[index];
-          return InkWell(
-            onTap: () {
-              setState(() {
-                contact.selected = !contact.selected;
-              });
-            },
-            child: ContactGroupCard(contact: contact),
-          );
-        },
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.only(top: 75),
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: contacts.length,
+                  itemBuilder: (context, index) {
+                    final contact = contacts[index];
+                    return InkWell(
+                      onTap: () {
+                        if (contact.selected == false) {
+                          addToGroup(contact);
+                        } else {
+                          removeFromGroup(contact);
+                        }
+                      },
+                      child: ContactGroupCard(contact: contact),
+                    );
+                  }),
+            ),
+          ),
+
+          group.isNotEmpty
+              ? Container(
+                  height: 75,
+                  width: double.infinity,
+                  color: Colors.white,
+                  padding: const EdgeInsets.only(top: 10, left: 14),
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: group.length,
+                    scrollDirection: Axis.horizontal,
+                    separatorBuilder: (context, index) => const SizedBox(
+                      width: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      final contact = group[index];
+                      return contact.imgUrl != null &&
+                              contact.imgUrl!.isNotEmpty
+                          ? Column(
+                              children: [
+                                Stack(
+                                  children: [
+                                    ClipOval(
+                                      child: Image.network(
+                                        contact.imgUrl!,
+                                        width: 48,
+                                        height: 48,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      right: 0,
+                                      child: InkWell(
+                                        onTap: () => removeFromGroup(contact),
+                                        child: const CircleAvatar(
+                                          backgroundColor: Colors.white,
+                                          radius: 12,
+                                          child: Icon(Icons.cancel,
+                                              color: Colors.grey),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(contact.name!),
+                              ],
+                            )
+                          : Column(
+                              children: [
+                                Stack(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.blueGrey.shade300,
+                                      radius: 23,
+                                      child: const Icon(Icons.person,
+                                          color: Colors.white),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      right: 0,
+                                      child: InkWell(
+                                        onTap: () => removeFromGroup(contact),
+                                        child: const CircleAvatar(
+                                          backgroundColor: Colors.white,
+                                          radius: 12,
+                                          child: Icon(Icons.cancel,
+                                              color: Colors.grey),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 1),
+                                Text(
+                                  contact.name!,
+                                  style: const TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            );
+                    },
+                  ),
+                )
+              : Container(height: 75, color: Colors.white), // SizedBox
+        ],
       ),
     );
   }
